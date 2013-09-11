c40cd36a5a062104772976d45bb353702a48c9bc
Martin N. <mr.avgp@googlemail.com>
2013-09-11 07:53:10 +0200

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+echo
+echo "Installing Gitreceive"
+echo
+
+git clone https://github.com/progrium/gitreceive /usr/local/bin/gitreceive
+gitreceive init
+
+echo
+echo "Installing Eternity"
+echo
+
+cd /home/git
+git clone https://github.com/AVGP/eternity.git
+cd eternity && npm install
+chown -R git:git eternity
+
+echo
+echo "Setting up paths..."
+echo
+mkdir /var/repos
+chown -R git:git /var/repos
+
+echo 
+echo "How to add your SSH key:"
+echo 'cat ~/.ssh/id_rsa.pub | ssh you@yourserver.com "sudo gitreceive upload-key you"'
+echo
